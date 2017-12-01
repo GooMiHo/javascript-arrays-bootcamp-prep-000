cff0382ca9c3d405add108893ac106cb9b2019d7
Bethany  Walker <bethanymarciawalker@outlook.com>
2017-12-01 04:38:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, element){}
+function addElementToBeginningOfArray(array, element){
   var array = ['cat', 'dog' 'bird'];
   var element = 'fish';
   return [element, ...array];
+}
